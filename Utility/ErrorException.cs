004c509cfd58af4c171a91db8e21e188b8a503ba
Carnagion <carnagion.games@gmail.com>
2022-08-03 14:29:35 +0200

    Fix documentation comment references

--- Utility/ErrorException.cs
+++ Utility/ErrorException.cs
@@ -5,22 +5,22 @@ using JetBrains.Annotations;
 namespace Godot.Utility
 {
     /// <summary>
-    /// A wrapper around <see cref="Error"/> to make it throwable.
+    /// A wrapper around <see cref="Godot.Error"/> to make it throwable.
     /// </summary>
     [PublicAPI]
     public class ErrorException : Exception
     {
         /// <summary>
-        /// Initialises a new <see cref="ErrorException"/> with the specified <see cref="Error"/>.
+        /// Initialises a new <see cref="ErrorException"/> with the specified <see cref="Godot.Error"/>.
         /// </summary>
-        /// <param name="error">The Godot <see cref="Error"/>.</param>
+        /// <param name="error">The Godot <see cref="Godot.Error"/>.</param>
         public ErrorException(Error error) : base(error.ToString())
         {
             this.Error = error;
         }
         
         /// <summary>
-        /// The Godot <see cref="Error"/>.
+        /// The Godot <see cref="Godot.Error"/>.
         /// </summary>
         public Error Error
         {
