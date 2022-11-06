b0b3b01833454c4394f73032c65593a94a1032be
Carnagion <carnagion.games@gmail.com>
2022-11-06 14:47:26 +0000

    Add method for copying directory contents

--- GDScript/utility/extensions/directory_extensions.gd
+++ GDScript/utility/extensions/directory_extensions.gd
@@ -1,6 +1,33 @@
 ## Contains utility methods for [Directory].
 class_name DirectoryExtensions
 
+## Copies all files from the directory at [code]from[/code] to the directory at [code]to[/code], recursively if specified.
+static func copy_contents(directory, from, to, recursive = false):
+	directory.open(from)
+	# Create destination directory if it doesn't already exist
+	directory.make_dir_recursive(to)
+	var copied = []
+	# Copy all files inside the source directory non-recursively
+	for from_file in directory.get_files():
+		from_file = from.path_join(from_file)
+		var to_file = StringExtensions.replace_once(from_file, from, to)
+		directory.copy(from_file, to_file)
+		copied.append(to_file)
+	if not recursive:
+		return copied
+	# Copy all files recursively
+	for from_sub_directory in DirectoryExtensions.get_directories_recursive(directory):
+		var to_sub_directory = StringExtensions.replace_once(from_sub_directory, from, to)
+		directory.make_dir_recursive(to_sub_directory)
+		var inner_directory = Directory.new()
+		inner_directory.open(from_sub_directory)
+		for from_file in inner_directory.get_files():
+			from_file = from_sub_directory.path_join(from_file)
+			var to_file = StringExtensions.replace_once(from_file, from, to)
+			directory.copy(from_file, to_file)
+			copied.append(to_file)
+	return copied
+
 ## Returns the complete paths of all subdirectories inside [code]directory[/code], searching recursively.
 static func get_directories_recursive(directory):
 	var directories = []
